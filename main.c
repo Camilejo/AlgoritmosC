0e3227982cca17bf50f63d4e64a05c80707a0c20
Camilejo <camiloalejocc@gmail.com>
2022-12-23 14:10:14 -0500

    Añadiendo el menu, creando metodos y añadiendo borrar espacios

--- main.c
+++ main.c
@@ -3,23 +3,79 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(void) {
+int numeroRomano();
 
-    //Numeros Romanos
+void numeroMagico();
 
+int multiplicacionMatrices();
+
+void borrarEspacios();
+
+int main() {
+
+
+    /**
+    int option = 0;
+
+    do {
+        printf("---MENU DE FUNCIONES--- \n"
+               "1. Numeros Romanos \n"
+               "2. Factores primos \n"
+               "3. Borrar espacios \n"
+               "4. Numeros egolatras \n"
+               "5. Numero Magico \n"
+               "6. Fechas \n"
+               "7. Producto Punto \n"
+               "8. Multiplicacion de matrices \n"
+               "9. Matriz Magica \n"
+               "10. salir \n"
+               "Introduzca una opcion: \n");
+
+        scanf("%d", &option);
+
+        switch (option) {
+            case 1:
+                numeroRomano();
+                break;
+            case 2:
+                break;
+            case 3:
+                borrarEspacios();
+                break;
+            case 4:
+                break;
+            case 5:
+                numeroMagico();
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                multiplicacionMatrices();
+                break;
+            case 9:
+                break;
+        }
+    } while (option != 10);
+     **/
+}
+
+//Numeros Romanos
+int numeroRomano() {
     char roman_numeral[16];
     int decimal = 0;
 
     printf("Ingrese un numero romano: ");
     scanf("%s", roman_numeral);
-    int caracter = strlen(roman_numeral) -1;
+    int caracter = strlen(roman_numeral) - 1;
 
     for (int i = 0; i < strlen(roman_numeral); i++) {
         switch (roman_numeral[i]) {
             case 'I':
-                if(caracter == i){
+                if (caracter == i) {
                     decimal += 1;
-                }else{
+                } else {
                     decimal -= 1;
                 }
                 break;
@@ -49,10 +105,11 @@ int main(void) {
     }
 
     printf("El numero romano es equivalente a %d en base 10.\n", decimal);
+}
 
-    //Numeros magicos
-
-    int num, a, b, c, d, e,ascendente,descendente,resta;
+//Numeros magicos
+void numeroMagico() {
+    int num, a, b, c, d, e, ascendente, descendente, resta;
 
     printf("Ingrese un numero de 3 digitos: ");
     scanf("%d", &num);
@@ -76,11 +133,11 @@ int main(void) {
         a = b;
         b = d;
     }
-    a=a*100;
-    b=b*10;
-    ascendente= a + b + c;
-    a=a/100;
-    b=b/10;
+    a = a * 100;
+    b = b * 10;
+    ascendente = a + b + c;
+    a = a / 100;
+    b = b / 10;
     printf("Ascendente: %d%d%d\n", a, b, c);
 
     // Ordenar los dígitos de forma descendente
@@ -99,21 +156,23 @@ int main(void) {
         a = b;
         b = d;
     }
-    a=a*100;
-    b=b*10;
-    descendente= a + b + c;
-    a=a/100;
-    b=b/10;
+    a = a * 100;
+    b = b * 10;
+    descendente = a + b + c;
+    a = a / 100;
+    b = b / 10;
     printf("Descendente: %d%d%d\n", a, b, c);
-    resta=descendente-ascendente;
-    printf("Resta: %d\n",resta);
-    if(resta==num){
+    resta = descendente - ascendente;
+    printf("Resta: %d\n", resta);
+    if (resta == num) {
         printf("El numero es magico");
-    }else{
-        printf("El numero no es magico");
+    } else {
+        printf("El numero no es magico\n");
     }
+}
 
-    //Multiplicación de matrices
+//Multiplicación de matrices
+int multiplicacionMatrices() {
     int i, j, k;
     int filas1, columnas1, filas2, columnas2;
     int matriz1[100][100], matriz2[100][100], resultado[100][100];
@@ -137,13 +196,13 @@ int main(void) {
 
     for (i = 0; i < filas1; i++) {
         for (j = 0; j < columnas1; j++) {
-            matriz1[i][j] = rand()%11 +1;
+            matriz1[i][j] = rand() % 11 + 1;
         }
     }
 
     for (i = 0; i < filas2; i++) {
         for (j = 0; j < columnas2; j++) {
-            matriz2[i][j] = rand()%11 +1;
+            matriz2[i][j] = rand() % 11 + 1;
         }
     }
 
@@ -170,3 +229,26 @@ int main(void) {
     return 0;
 }
 
+//Borrar espacios
+void borrarEspacios() {
+
+    char cadena[60];
+    char stringSalida[60];
+    printf("Ingrese un texto corto: \n");
+    scanf("%[^\n]", cadena);
+
+    int auxIndex = 0;
+    for (int i = 0; i < strlen(cadena); ++i) {
+        if(cadena[i] != *" "){
+            stringSalida[auxIndex] = cadena[i];
+            auxIndex++;
+        }
+    }
+
+    printf("Texto sin espacios: \n");
+    printf("%s", stringSalida);
+}
+
+void fecha() {
+
+}
